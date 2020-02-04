f71ab206da6836664f2c3f939cbd5c72e97c9f44
Olga Novichkova <olganovichkova3@gmail.com>
2020-02-03 21:25:49 -0700

    List boxes

--- src/FormPanel.java
+++ src/FormPanel.java
@@ -12,6 +12,7 @@ public class FormPanel extends JPanel {
     private JTextField occupationField;
     private JButton okBtn;
     private FormListener formListener;
+    private JList ageList;
 
     public FormPanel(){
         Dimension dim = getPreferredSize();
@@ -22,6 +23,17 @@ public class FormPanel extends JPanel {
         occupationLabel = new JLabel("Occupation: ");
         nameField = new JTextField(10);
         occupationField = new JTextField(10);
+        ageList = new JList();
+
+        DefaultListModel ageModel = new DefaultListModel();
+        ageModel.addElement("Under 18");
+        ageModel.addElement("18 to 65");
+        ageModel.addElement("65 and over");
+        ageList.setModel(ageModel);
+
+        ageList.setPreferredSize(new Dimension(110, 70));
+        ageList.setBorder(BorderFactory.createEtchedBorder());
+        ageList.setSelectedIndex(1);
 
         okBtn = new JButton("OK");
 
@@ -29,6 +41,9 @@ public class FormPanel extends JPanel {
             public void actionPerformed(ActionEvent actionEvent) {
                 String name = nameField.getText();
                 String occupation = occupationField.getText();
+                String ageCat = (String)ageList.getSelectedValue();
+
+                System.out.println(ageCat);
 
                 FormEvent ev = new FormEvent(this, name, occupation);
 
@@ -85,9 +100,20 @@ public class FormPanel extends JPanel {
         /////////Third row//////////////
 
         gc.weightx = 1;
-        gc.weighty = 2.0;
+        gc.weighty = 0.2;
 
         gc.gridy = 2;
+        gc.gridx = 1;
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
+        add(ageList, gc);
+
+
+        /////////Fourth row//////////////
+
+        gc.weightx = 1;
+        gc.weighty = 2.0;
+
+        gc.gridy = 3;
         gc.gridx = 1;
         gc.anchor = GridBagConstraints.FIRST_LINE_START;
         add(okBtn, gc);
